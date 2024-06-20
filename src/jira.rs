2b6ba1859b16ad38f7c36c2e2c85b289fe1eb967
Erik Karsten <erik@opcode.xyz>
2024-06-20 17:02:15 +0300

    feat: nicer errors for env values

--- src/jira.rs
+++ src/jira.rs
@@ -1,5 +1,5 @@
 use gouqi::{Credentials, Jira};
-use anyhow::Error;
+use anyhow::{Context, Result};
 use std::env;
 
 pub struct JiraClient {
@@ -7,17 +7,17 @@ pub struct JiraClient {
 }
 
 impl JiraClient {
-    pub fn new() -> Result<Self, Error> {
-        let host = env::var("JIRA_HOST")?;
-        let username = env::var("JIRA_USERNAME")?;
-        let jira_token = env::var("JIRA_API_TOKEN")?;
+    pub fn new() -> Result<Self> {
+        let host = env::var("JIRA_HOST").context("JIRA_HOST not defined")?;
+        let username = env::var("JIRA_USERNAME").context("JIRA_USERNAME not defined")?;
+        let jira_token = env::var("JIRA_API_TOKEN").context("JIRA_API_TOKEN not defined")?;
 
         let credentials = Credentials::Basic(username, jira_token);
         let client = Jira::new(host, credentials)?;
         Ok(Self { client })
     }
 
-    pub fn asd(&self) -> Result<(), Error> {
+    pub fn asd(&self) -> Result<()> {
         match self.client.search().iter("created >= -30d AND assignee = currentUser() ORDER BY created DESC", &Default::default()) {
             Ok(results) => {
                 for issue in results {
