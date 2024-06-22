8fdd874a6683e2859a31934d5a0c180b489d5d0e
Erik Karsten <erik@opcode.xyz>
2024-06-22 13:21:27 +0300

    feat: move AI url endpoint to variable

--- src/ai.rs
+++ src/ai.rs
@@ -4,7 +4,7 @@ use std::env;
 
 pub fn choose_ticket() -> String {
     let auth = Auth::new(&env::var("AI_KEY").unwrap());
-    let openai = OpenAI::new(auth, "***REMOVED***");
+    let openai = OpenAI::new(auth, &env::var("AI_HOST").unwrap());
     let body = ChatBody {
         model: "llama3".to_string(),
         max_tokens: Some(256),
